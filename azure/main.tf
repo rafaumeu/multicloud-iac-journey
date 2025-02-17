4e074384a978dc43c750c754973c09e7e6269e1e
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-02-17 12:37:14 -0300

    chore(providers): update cloud providers configuration
    - Update GCP provider configuration with project and region
    - Add Azure provider with resource group and vnet setup
    - Configure provider versions and required settings
    - Improve resource organization across cloud providers
    
    closes #4

--- azure/main.tf
+++ azure/main.tf
@@ -1,7 +1,13 @@
 resource "azurerm_resource_group" "rg" {
-  name = "rafael-hmg"
+  name     = "rafael-hmg"
   location = "brazilsouth"
   tags = {
     Iac = "True"
   }
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rafael-vnet"
+  location            = "brazilsouth"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
